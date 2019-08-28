cd9892ed2b44f6d48fca8315914faf85a22b5684
Aleymarth <aleymarth@gmail.com>
2019-08-28 01:23:07 -0500

    Identity
    Se agrego la identidad y la base de datos

---
+++ Areas/Identity/Data/LletraWebContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LletraWeb.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LletraWeb.Models
+{
+    public class LletraWebContext : IdentityDbContext<LletraWebUser>
+    {
+        public LletraWebContext(DbContextOptions<LletraWebContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LletraWeb.Areas.Identity.Data;
+using LletraWeb.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LletraWeb.Areas.Identity.IdentityHostingStartup))]
+namespace LletraWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<LletraWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("LletraWebContextConnection")));
+
+                services.AddDefaultIdentity<LletraWebUser>()
+                    .AddEntityFrameworkStores<LletraWebContext>();
+            });
+        }
+    }
+}
